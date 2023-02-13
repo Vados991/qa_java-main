388d34a78c5bcff53092217c0514f4b53f011ed0
Vados <vados210@yandex.ru>
2023-02-14 01:21:58 +0300

    Отредактировал тесты + доп задание. Удалил лишний тест

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -4,7 +4,8 @@ import java.util.List;
 
 public class Lion {
     boolean hasMane;
-    public Lion(String sex) throws Exception {
+    Feline feline;
+    public Lion(String sex, Feline feline) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
@@ -12,11 +13,9 @@ public class Lion {
         } else {
             throw new Exception("Используйте допустимые значения пола животного - самец или самка");
         }
+        this.feline = feline;
     }
-    Feline feline = new Feline();
-    public Lion(String sex, Feline feline) {
 
-    }
     public int getKittens() {
         return feline.getKittens();
     }
--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -3,16 +3,21 @@ import com.example.Lion;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
-
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
-
+    String sex;
+@Mock
     Feline feline;
     Lion lion;
 
     @Before
+
     public void setUp() throws Exception {
         feline = Mockito.mock(Feline.class);
         Mockito.when(feline.getKittens()).thenReturn(1);
@@ -30,6 +35,12 @@ public class LionTest {
     @Test
     public void getFood() throws Exception {
         List<String> expected = List.of("Животные", "Птицы", "Рыба");
-        Assert.assertEquals("У льва хищника", expected, lion.getFood());
+        Assert.assertEquals("У льва диета хищника", expected, lion.getFood());
+    }
+    @Test(expected = Exception.class)
+    public void lionExceptionTest() throws Exception {
+        Lion lion = new Lion(sex, feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        Assert.assertEquals(1, lion.getKittens());
     }
 }
